85c6e4310d2dbb3e292ee628dc2ba4d2913a6451
Eudivan Melo <eudivan44@gmail.com>
2022-12-13 17:43:03 -0300

    Update db_account.php

--- admin/connections/db_account.php
+++ admin/connections/db_account.php
@@ -1,5 +1,5 @@
 <?php
-    $con_account = new mysqli('54.233.140.224', 'eudivan', '11031997', 'db_account');
+    $con_account = new mysqli('localhost', 'root', '', 'db_account');
 
     if (!$con_account){
         die("Erro na conexão com o banco de dados! Código de erro: " . mysqli_connect_errno());
